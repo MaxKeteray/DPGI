e358910c6472961f52fa691c6f70aa28eb26134b
Maks Keteray <keteray23@gmail.com>
2025-05-13 23:14:59 +0300

    adding files from lab1

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Hello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Hello hello = new Hello();
+        hello.SayHello();
+    }
+}
